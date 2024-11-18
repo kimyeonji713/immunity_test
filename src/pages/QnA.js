ab894147e653aa384dbbf7b6b1c8f1f919e4ae57
kimyeonji713 <hfgj0014@naver.com>
2024-11-18 16:34:07 +0900

    qna 작성 중간저장

--- src/pages/QnA.js
+++ src/pages/QnA.js
@@ -1,3 +1,81 @@
-export const QnA = () => {
+export const QnA = ({ page, setPage, colorMode, userName }) => {
+  const questList = [
+    {
+      q: ["아침에 일어나기 힘들다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["상처가 잘 낫지 않는다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["감기에 자주 걸리고, 잘 낫지 않는다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["스트레스가 자주 쌓인다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["입안이 헐거나 입 주위에 물집이 생긴다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["눈에 염증이 자주 생긴다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["쉽게 피곤하고 피로가 잘 풀리지 않는다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["체력이 급격히 떨어짐을 느낀다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["사소한 일에도 짜증이 난다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["다이어트를 자주한다."],
+      a: [
+        { text: "그렇다.", score: 0 },
+        { text: "아니다.", score: 1 },
+      ],
+    },
+    {
+      q: ["테스트가 모두 끝났어요!"],
+      a: [{ text: "결과 보러 가기!", score: 0 }],
+    },
+  ];
+
+  const [counts, setCounts] = useState({ score: 0 });
   return <div>QnA</div>;
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,16 +2,25 @@ import { useState } from "react";
 import { useScrollTop } from "../../lib/useScrollTop";
 import { useForm } from "react-hook-form";
 import { PageTitle } from "../../components/PageTitle";
-import { Box, Button, Container, Heading, Img, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Img,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { QnA } from "../QnA";
 
 export const Home = () => {
   useScrollTop();
-  const { register, handleSubmit } = useForm();
 
   const [page, setPage] = useState(0);
   const [userName, setUserName] = useState("나");
 
+  const { colorMode } = useColorMode();
   const [isLoading, setIsLoading] = useState(true);
 
   const mainImgUrl =
@@ -20,47 +29,56 @@ export const Home = () => {
   return (
     <>
       <PageTitle title={"Home"} />
-      <Container
-        maxW={"500px"}
-        minxH={"100vh"}
-        w={"100%"}
-        h={"100vh"}
-        mx={"auto"}
-        bgColor={"#fff"}
-        padding={"40px 10px"}
-      >
-        <Box marginTop={"40px"}>
-          <Heading textAlign={"center"} fontSize={"55px"} color={"#008433"}>
-            내 면역력은 <br />
-            괜찮을까?
-          </Heading>
+      {page === 0 ? (
+        <Container
+          maxW={"500px"}
+          minxH={"100vh"}
+          w={"100%"}
+          h={"100vh"}
+          mx={"auto"}
+          bgColor={"#fff"}
+          padding={"40px 10px"}
+        >
+          <Box marginTop={"40px"}>
+            <Heading textAlign={"center"} fontSize={"55px"} color={"#008433"}>
+              내 면역력은 <br />
+              괜찮을까?
+            </Heading>
 
-          <Heading
-            textAlign={"center"}
-            fontSize={"30px"}
-            fontWeight={"900"}
-            color={"#333333"}
-            marginTop={"20px"}
-          >
-            #면역력 테스트
-          </Heading>
-          <Img w={"100%"} src={mainImgUrl} marginTop={"30px"} />
-        </Box>
+            <Heading
+              textAlign={"center"}
+              fontSize={"30px"}
+              fontWeight={"900"}
+              color={"#333333"}
+              marginTop={"20px"}
+            >
+              #면역력 테스트
+            </Heading>
+            <Img w={"100%"} src={mainImgUrl} marginTop={"30px"} />
+          </Box>
 
-        <Link to={"/qna"}>
-          <Button
-            w={"150px"}
-            h={"50px"}
-            bgColor={"#008433"}
-            color={"#fff"}
-            marginLeft={"155px"}
-            marginTop={"20px"}
-            colorScheme="none"
-          >
-            테스트 시작!
-          </Button>
-        </Link>
-      </Container>
+          <Link to={"/qna"}>
+            <Button
+              w={"150px"}
+              h={"50px"}
+              bgColor={"#008433"}
+              color={"#fff"}
+              marginLeft={"155px"}
+              marginTop={"20px"}
+              colorScheme="none"
+            >
+              테스트 시작!
+            </Button>
+          </Link>
+        </Container>
+      ) : (
+        <QnA
+          page={page}
+          setPage={setPage}
+          colorMode={colorMode}
+          name={userName}
+        />
+      )}
     </>
   );
 };
